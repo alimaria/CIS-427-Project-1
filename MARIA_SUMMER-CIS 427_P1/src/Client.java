df5f770d2f30ae41c7ad9b0003708e2581eb7242
alimaria <98565310+alimaria@users.noreply.github.com>
2022-02-13 20:25:58 -0500

    Update Client.java

--- MARIA_SUMMER-CIS 427_P1/src/Client.java
+++ MARIA_SUMMER-CIS 427_P1/src/Client.java
@@ -1,15 +1,18 @@
 import java.net.*;
 import java.io.*;
 
+//Client class, created to connect with the server class, can't run without it
+
 public class Client {
 
     public static void main(String args[]) {
         try {
-            Socket s = new Socket("localhost", 9034);
-            DataInputStream din = new DataInputStream(s.getInputStream());
-            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            Socket s = new Socket("localhost", 9034); //connecting to port
+            DataInputStream din = new DataInputStream(s.getInputStream()); //creating input variable
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream()); //creating output variable
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+            
+            //while loop to check connection
             String command = "", response = "";
             while (true) {
                 command = br.readLine();
